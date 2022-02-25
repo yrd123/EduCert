ba2a42ec310474475fea96a55cdab6ea280ab09e
Sanyam Gandhi <sanyamgandhi00@gmail.com>
2022-02-25 16:00:18 +0530

    Validation

--- src/Components/membershipService.jsx
+++ src/Components/membershipService.jsx
@@ -4,8 +4,107 @@ import Navbar from './navbar';
 
 export default class MembershipService extends Component {
     state = {
-        orginfo: { name: '', address: '', email: '', password: '', type: '', contact: '' }
+        orginfo: { name: '', address: '', email: '', password: '', type: '', contact: '' } ,
+        errors : {} 
     };
+
+    handleValidation() {
+        let orginfo = this.state.orginfo;
+        let errors = {};
+        let formIsValid = true;
+    
+        //Name
+        if (!orginfo["name"]) {
+          formIsValid = false;
+          errors["name"] = "Name Cannot be empty";
+        }
+    
+        if (typeof orginfo["name"] !== "undefined") {
+          if (!orginfo["name"].match(/^[a-zA-Z][a-zA-Z ]*$/)) {
+            formIsValid = false;
+            errors["name"] = "Organization Name should contain only Letters and spaces";
+          }
+        }
+
+        //Address
+        if (!orginfo["address"]) {
+            formIsValid = false;
+            errors["address"] = "Address Cannot be empty";
+          }
+      
+          if (typeof orginfo["address"] !== "undefined") {
+            if (!orginfo["address"].match(/^[a-zA-Z ]$/)) {
+              formIsValid = false;
+              errors["address"] = "Organization Address should contain only Letters and spaces";
+            }
+          }
+      
+    
+        //Email
+        if (!orginfo["email"]) {
+          formIsValid = false;
+          errors["email"] = "Cannot be empty";
+        }
+    
+        if (typeof orginfo["email"] !== "undefined") {
+          let lastAtPos = orginfo["email"].lastIndexOf("@");
+          let lastDotPos = orginfo["email"].lastIndexOf(".");
+    
+          if (
+            !(
+              lastAtPos < lastDotPos &&
+              lastAtPos > 0 &&
+              orginfo["email"].indexOf("@") === -1 &&
+              lastDotPos > 2 &&
+              orginfo["email"].length - lastDotPos > 2
+            )
+          ) {
+            formIsValid = false;
+            errors["email"] = "Email is not valid";
+          }
+        }
+        
+
+        //Password 
+        if (!orginfo["password"]) {
+            formIsValid = false;
+            errors["password"] = "Cannot be empty";
+          }
+      
+          if (typeof orginfo["password"] !== "undefined") {
+            var minNumberofChars = 6;
+            var maxNumberofChars = 16;
+            var newPassword = orginfo["password"] ;
+            if(newPassword.length < minNumberofChars || newPassword.length > maxNumberofChars){
+                formIsValid = false;
+              errors["password"] = "Password should be between 6 to 16 char";
+            }
+            if (!orginfo["password"].match(/^[a-zA-Z0-9!@#$%^&*]{6,16}$/) ) 
+            {
+              formIsValid = false;
+              errors["password"] = "password should contain atleast one number and one special character";
+            }
+          }
+
+          //Contact 
+          if (!orginfo["contact"]) {
+            formIsValid = false;
+            errors["contact"] = "Contact Cannot be empty";
+          }
+      
+          if (typeof orginfo["contact"] !== "undefined") {
+            if (!orginfo["contact"].match(/^[0-9]$/)) {
+              formIsValid = false;
+              errors["contact"] = "Organization Contact should be a 10 Digit No";
+            }
+          }
+  
+      
+      
+        this.setState({ errors: errors });
+        return formIsValid;
+      } ;
+    
 
            
 
@@ -18,6 +117,19 @@ export default class MembershipService extends Component {
         console.log(orginfo) ;
     } ;
 
+    handleSubmit = e =>{
+        e.preventDefault();
+
+    if (this.handleValidation()) {
+      
+    } else {
+        return ;
+    }
+
+    console.log(this.state.errors);
+
+    } ;
+
     render() {
         return (
             <React.Fragment>
@@ -25,7 +137,7 @@ export default class MembershipService extends Component {
                 <div className="loginDiv" style={{}}>
                     <div className="forms">
                         {/* LOGIN FORM */}
-                        <form id="login" onsubmit="" name="membershipserviceform" >
+                        <form id="login" onSubmit={this.handleSubmit}  name="membershipserviceform" >
                             <br />
                             <div id="loginwarning" className="warning">
                                 {/* INSERT LOGIN WARNINGS HERE */}
@@ -33,11 +145,20 @@ export default class MembershipService extends Component {
                             <div className="input-field">
                                 <label htmlFor="name">Organization Name</label>
                                 <input type="text" onChange = {this.handleChange} value = {this.state.orginfo.name} id="name"  name="name" placeholder="kjsce1221" />
-
+                                
+                                
+                                <div class="alert alert-danger" role="alert">
+                                {  this.state.errors["name"]}
+                                </div>
+                                
+              
 
                                 <label htmlFor="address">Organization Address</label>
                                 <input type="textarea" onChange = {this.handleChange} value = {this.state.orginfo.address} id="address" name="address" placeholder="Vidyavihar Mumbai" />
 
+                                <div class="alert alert-danger" role="alert">
+                                {  this.state.errors["address"]}
+                                </div>
 
 
                                 <label htmlFor="type">Organization Type</label>
@@ -53,17 +174,23 @@ export default class MembershipService extends Component {
 
                                 <label htmlFor="email">Organization Email</label>
                                 <input type="text" onChange = {this.handleChange} value = {this.state.orginfo.email} id="email" name="email" placeholder="kjsce1221@somaiya.edu" />
-
+                                <div class="alert alert-danger" role="alert">
+                                {  this.state.errors["email"]}
+                                </div>
 
 
                                 <label htmlFor="password">Organization Password</label>
                                 <input type="password" onChange = {this.handleChange} value = {this.state.orginfo.password} id="password"  name="password" placeholder="********" />
-
+                                <div class="alert alert-danger" role="alert">
+                                {  this.state.errors["password"]}
+                                </div>
 
 
                                 <label htmlFor="contact">Organization Contact No</label>
                                 <input type="number" onChange = {this.handleChange} value = {this.state.orginfo.contact} id="contact" name="contact" placeholder="7887779499" />
-
+                                <div class="alert alert-danger" role="alert">
+                                {  this.state.errors["contact"]}
+                                </div>
 
                                 <input type="submit" value="ADD ORGANIZATION" defaultValue="ADD ORGANIZATION" className="button" />
                             </div>
