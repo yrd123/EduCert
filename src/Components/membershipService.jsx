62f5e2373cc7ff870bedf48a63dc3f65dcc2d34c
Sanyam Gandhi <sanyamgandhi00@gmail.com>
2022-02-26 13:47:03 +0530

    Validation

--- src/Components/membershipService.jsx
+++ src/Components/membershipService.jsx
@@ -19,7 +19,7 @@ export default class MembershipService extends Component {
           errors["name"] = "Name Cannot be empty";
         }
     
-        if (typeof orginfo["name"] !== "undefined") {
+        else if (typeof orginfo["name"] !== "undefined") {
           if (!orginfo["name"].match(/^[a-zA-Z][a-zA-Z ]*$/)) {
             formIsValid = false;
             errors["name"] = "Organization Name should contain only Letters and spaces";
@@ -31,9 +31,8 @@ export default class MembershipService extends Component {
             formIsValid = false;
             errors["address"] = "Address Cannot be empty";
           }
-      
-          if (typeof orginfo["address"] !== "undefined") {
-            if (!orginfo["address"].match(/^[a-zA-Z ]$/)) {
+          else if (typeof orginfo["address"] !== "undefined") {
+            if (!orginfo["address"].match(/^[a-zA-Z ]*$/)) {
               formIsValid = false;
               errors["address"] = "Organization Address should contain only Letters and spaces";
             }
@@ -46,7 +45,7 @@ export default class MembershipService extends Component {
           errors["email"] = "Cannot be empty";
         }
     
-        if (typeof orginfo["email"] !== "undefined") {
+        else if (typeof orginfo["email"] !== "undefined") {
           let lastAtPos = orginfo["email"].lastIndexOf("@");
           let lastDotPos = orginfo["email"].lastIndexOf(".");
     
@@ -71,7 +70,7 @@ export default class MembershipService extends Component {
             errors["password"] = "Cannot be empty";
           }
       
-          if (typeof orginfo["password"] !== "undefined") {
+          else if (typeof orginfo["password"] !== "undefined") {
             var minNumberofChars = 6;
             var maxNumberofChars = 16;
             var newPassword = orginfo["password"] ;
@@ -92,7 +91,7 @@ export default class MembershipService extends Component {
             errors["contact"] = "Contact Cannot be empty";
           }
       
-          if (typeof orginfo["contact"] !== "undefined") {
+          else if (typeof orginfo["contact"] !== "undefined") {
             if (!orginfo["contact"].match(/^[0-9]$/)) {
               formIsValid = false;
               errors["contact"] = "Organization Contact should be a 10 Digit No";
@@ -146,20 +145,23 @@ export default class MembershipService extends Component {
                                 <label htmlFor="name">Organization Name</label>
                                 <input type="text" onChange = {this.handleChange} value = {this.state.orginfo.name} id="name"  name="name" placeholder="kjsce1221" />
                                 
-                                
+                                { this.state.errors['name'] && 
                                 <div class="alert alert-danger" role="alert">
                                 {  this.state.errors["name"]}
                                 </div>
+                                }
                                 
               
 
                                 <label htmlFor="address">Organization Address</label>
                                 <input type="textarea" onChange = {this.handleChange} value = {this.state.orginfo.address} id="address" name="address" placeholder="Vidyavihar Mumbai" />
-
+                                
+                                { this.state.errors["address"] && 
                                 <div class="alert alert-danger" role="alert">
-                                {  this.state.errors["address"]}
+                                {  this.state.errors["address"] }
                                 </div>
-
+                                }
+                                
 
                                 <label htmlFor="type">Organization Type</label>
                                 <select className="custom-select" onChange = {this.handleChange} value = {this.state.orginfo.type} id="type"  name="type" id="cars">
@@ -174,23 +176,34 @@ export default class MembershipService extends Component {
 
                                 <label htmlFor="email">Organization Email</label>
                                 <input type="text" onChange = {this.handleChange} value = {this.state.orginfo.email} id="email" name="email" placeholder="kjsce1221@somaiya.edu" />
+                                
+
+                                
+                                { this.state.errors["email"] && 
                                 <div class="alert alert-danger" role="alert">
                                 {  this.state.errors["email"]}
                                 </div>
+                                }
+
+                                
 
 
                                 <label htmlFor="password">Organization Password</label>
                                 <input type="password" onChange = {this.handleChange} value = {this.state.orginfo.password} id="password"  name="password" placeholder="********" />
+                                { this.state.errors["password"] && 
                                 <div class="alert alert-danger" role="alert">
                                 {  this.state.errors["password"]}
                                 </div>
+                                }
 
 
                                 <label htmlFor="contact">Organization Contact No</label>
                                 <input type="number" onChange = {this.handleChange} value = {this.state.orginfo.contact} id="contact" name="contact" placeholder="7887779499" />
+                                { this.state.errors['contact'] && 
                                 <div class="alert alert-danger" role="alert">
                                 {  this.state.errors["contact"]}
                                 </div>
+                                }
 
                                 <input type="submit" value="ADD ORGANIZATION" defaultValue="ADD ORGANIZATION" className="button" />
                             </div>
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -3,52 +3,52 @@ import { NavLink } from 'react-router-dom';
 import './navbar.css';
 
 class Navbar extends Component {
-    render() { 
-        return (
-        /*<nav className="navbar">
-          <div className="container">
-            <div className="navbar-header">
-              <button className="navbar-toggler" data-toggle="open-navbar1">
-                <span />
-                <span />
-                <span />
-              </button>
-              <a href="#">
-                <h4>Block<span>CERT</span></h4>
-              </a>
-            </div>
-            <div className="navbar-menu" id="open-navbar1">
-              <ul className="navbar-nav">
-                <li className="active">
-                  <a href="index.html">Home</a>
-                </li>
-                <li><a href="/organization/login">Organization</a></li>
-                <li className="navbar-dropdown">
-                  <a href="/applicant/login" className="dropdown-toggler" data-dropdown="my-dropdown-id">
-                  Applicant
-                  </a>
-                </li>
-                <li><a href="{% url 'profile' %}">About Us</a></li>
-                <li><a href="/applicant/dashboard">Applicant Dashboard</a></li>
-                <li><a href="/organization/dashboard">Organization Dashboard</a></li>
-              </ul>
-            </div>
+  render() {
+    return (
+      /*<nav className="navbar">
+        <div className="container">
+          <div className="navbar-header">
+            <button className="navbar-toggler" data-toggle="open-navbar1">
+              <span />
+              <span />
+              <span />
+            </button>
+            <a href="#">
+              <h4>Block<span>CERT</span></h4>
+            </a>
           </div>
-        </nav>*/
+          <div className="navbar-menu" id="open-navbar1">
+            <ul className="navbar-nav">
+              <li className="active">
+                <a href="index.html">Home</a>
+              </li>
+              <li><a href="/organization/login">Organization</a></li>
+              <li className="navbar-dropdown">
+                <a href="/applicant/login" className="dropdown-toggler" data-dropdown="my-dropdown-id">
+                Applicant
+                </a>
+              </li>
+              <li><a href="{% url 'profile' %}">About Us</a></li>
+              <li><a href="/applicant/dashboard">Applicant Dashboard</a></li>
+              <li><a href="/organization/dashboard">Organization Dashboard</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>*/
 
-        <header>
-          <h1><img src="https://www.formationsco.com/wp-content/uploads/2017/12/certificate_icon.png" class="logoImg" /> EduCert</h1>
-          <div class="nav-div">
-          <NavLink to="/"> <button class="navBtn active">Home</button> </NavLink>
-            <NavLink to="/applicant/login"><button class="navBtn">Applicant</button></NavLink>
-            <NavLink to="/organization/login"><button class="navBtn">Organization</button></NavLink>
-            <NavLink to="/applicant/dashboard"><button class="navBtn">Applicant Dashboard</button></NavLink>
-            <NavLink to="/organization/dashboard"><button class="navBtn">Organization Dashboard</button></NavLink>
-           </div> 
-        </header>
-                
-        );
-    }
+      <header>
+        <h1><img src="https://www.formationsco.com/wp-content/uploads/2017/12/certificate_icon.png" class="logoImg" /> EduCert</h1>
+        <div class="nav-div">
+          <NavLink to="/"> <button class="navBtn ">Home</button></NavLink>
+          <NavLink to="/applicant/login"><button class="navBtn">Applicant</button></NavLink>
+          <NavLink to="/organization/login"><button class="navBtn">Organization</button></NavLink>
+          <NavLink to="/applicant/dashboard"><button class="navBtn">Applicant Dashboard</button></NavLink>
+          <NavLink to="/organization/dashboard"><button class="navBtn">Organization Dashboard</button></NavLink>
+        </div>
+      </header>
+
+    );
+  }
 }
- 
+
 export default Navbar;
