4aad6e091c23dd3c988feed79e950e88047a04af
Sanyam Gandhi <sanyamgandhi00@gmail.com>
2022-03-13 13:21:45 +0530

    validation for applicant signUp

--- src/Components/applicantLogin.jsx
+++ src/Components/applicantLogin.jsx
@@ -39,7 +39,7 @@ export default class ApplicantLogin extends Component {
   handleSubmitSignup = (e) => {
     e.preventDefault();
 
-    if (this.handleValidationSignup()) {
+    if (this.handleSignUpValidation()) {
     } else {
       return;
     }
@@ -104,6 +104,103 @@ export default class ApplicantLogin extends Component {
     return formIsValid;
   } ;
 
+
+  
+  handleSignUpValidation() {
+    let applicantinfoSignup = this.state.applicantinfoSignup;
+    let signupErrors = {};
+    let formIsValid = true;
+
+
+    //Email
+    if (!applicantinfoSignup["email"]) {
+      formIsValid = false;
+      signupErrors["email"] = "Cannot be empty";
+    }
+
+    else if (typeof applicantinfoSignup["email"] !== "undefined") {
+      let lastAtPos = applicantinfoSignup["email"].lastIndexOf("@");
+      let lastDotPos = applicantinfoSignup["email"].lastIndexOf(".");
+
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          applicantinfoSignup["email"].indexOf("@") === -1 &&
+          lastDotPos > 2 &&
+          applicantinfoSignup["email"].length - lastDotPos > 2
+        )
+      ) {
+        formIsValid = false;
+        signupErrors["email"] = "Email is not valid";
+      }
+    }
+
+    //Contact 
+    if (!applicantinfoSignup["contactNumber"]) {
+      formIsValid = false;
+      signupErrors["contactNumber"] = "Contact Cannot be empty";
+    }
+    else if (typeof applicantinfoSignup["contactNumber"] !== "undefined") {
+      if (!applicantinfoSignup["contactNumber"].match(/^[0-9]$/)) {
+        formIsValid = false;
+        signupErrors["contactNumber"] = "Contact should be a 10 Digit No";
+      }
+    }
+
+
+
+    
+    //fullName
+    if (!applicantinfoSignup["fullName"]) {
+      formIsValid = false;
+      signupErrors["fullName"] = "Name Cannot be empty";
+    }
+    else if (typeof applicantinfoSignup["fullName"] !== "undefined") {
+      if (!applicantinfoSignup["fullName"].match(/^[a-zA-Z][a-zA-Z ]*$/)) {
+        formIsValid = false;
+        signupErrors["fullName"] = "Organization Name should contain only Letters and spaces";
+      }
+    }
+
+    //address
+    if (!applicantinfoSignup["address"]) {
+      formIsValid = false;
+      signupErrors["address"] = "Cannot be empty";
+    }
+
+
+
+
+
+     //Password
+    if (!applicantinfoSignup["password"]) {
+      formIsValid = false;
+      signupErrors["password"] = "Cannot be empty";
+    } else if (typeof applicantinfoSignup["password"] !== "undefined") {
+      var minNumberofChars = 6;
+      var maxNumberofChars = 16;
+      var newPassword = applicantinfoSignup["password"];
+      if (
+        newPassword.length < minNumberofChars ||
+        newPassword.length > maxNumberofChars
+      ) {
+        formIsValid = false;
+        signupErrors["password"] = "Password should be between 6 to 16 char";
+      }
+      if (!applicantinfoSignup["password"].match(/^[a-zA-Z0-9!@#$%^&*]{6,16}$/)) {
+        formIsValid = false;
+        signupErrors["password"] =
+          "password should contain atleast one number and one special character";
+      }
+    }
+
+
+    this.setState({ signupErrors: signupErrors });
+    return formIsValid;
+  } ;
+
+
     render() {
       return  (
         <div>
@@ -149,17 +246,37 @@ export default class ApplicantLogin extends Component {
               </div>
             </form>
             {/* SIGNUP FORM */}
-            <form action="/signup" id="signup" style={{display: this.state.currentTab==='Sign Up' ? 'block' : 'none' }} onsubmit="return validatesignup()" name="signupform" method="POST">
+            <form onSubmit={this.handleSubmitSignup} id="signup" style={{display: this.state.currentTab==='Sign Up' ? 'block' : 'none' }} onsubmit="return validatesignup()" name="signupform" method="POST">
               <div id="signupwarning" className="warning">
                 {/* INSERT SIGNUP WARNINGS HERE */}
               </div>
               <div className="input-field">
                 <label htmlFor="email">Email</label>
                 <input type="email" id="email"name="email" onChange={this.handleChangeSignup} value = {this.state.applicantinfoSignup.email} placeholder="sanyamgandhi@gmail.com" />
+                {
+                  this.state.signupErrors["email"] &&
+                  <div class="alert alert-danger" role="alert">
+                    {this.state.signupErrors["email"]}
+                  </div>
+                }
                 <label htmlFor="fullName">Name</label>
                 <input type="text" id = "fullName" name="fullName" onChange={this.handleChangeSignup} value = {this.state.applicantinfoSignup.fullName} placeholder="John Doe" />
+                {
+                  this.state.signupErrors["fullName"] &&
+                  <div class="alert alert-danger" role="alert">
+                    {this.state.signupErrors["fullName"]}
+                  </div>
+                }
+                
                 <label htmlFor="address">Address</label>
                 <input type="text" id="address" name="address" onChange={this.handleChangeSignup} value = {this.state.applicantinfoSignup.address} placeholder="c8 sambhav appt" />
+                {
+                  this.state.signupErrors["address"] &&
+                  <div class="alert alert-danger" role="alert">
+                    {this.state.signupErrors["address"]}
+                  </div>
+                }
+                
                 <label htmlFor="pincode">Pin/Zip Code</label>
                 <input type="text" id="pincode" name="pincode" onChange={this.handleChangeSignup} value = {this.state.applicantinfoSignup.pincode} placeholder="John Doe" />
                 <label htmlFor="state">State</label>
@@ -168,6 +285,14 @@ export default class ApplicantLogin extends Component {
                 <input type="text" id = "country" name="country" onChange={this.handleChangeSignup} value = {this.state.applicantinfoSignup.country} placeholder="John Doe" />
                 <label htmlFor="contactNumber">Mobile Number</label>
                 <input type="number" id="contactNumber" name="contactNumber" onChange={this.handleChangeSignup} value = {this.state.applicantinfoSignup.contactNumber} placeholder={9876543210} />
+                {
+                  this.state.signupErrors["contactNumber"] &&
+                  <div class="alert alert-danger" role="alert">
+                    {this.state.signupErrors["contactNumber"]}
+                  </div>
+                }
+                
+                
                 <label htmlFor="dob">Date of Birth</label>
                 <input type="date" id="dob" name="dob" placeholder="22/12/2000" />
                 
