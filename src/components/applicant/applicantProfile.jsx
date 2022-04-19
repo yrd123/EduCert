d15a5bab2a1d04f3e695fc16b289d55ce7b1a7b5
pointres <ronakdesai159@gmail.com>
2022-04-19 20:38:12 +0530

    Connection 2

--- src/components/applicant/applicantProfile.jsx
+++ src/components/applicant/applicantProfile.jsx
@@ -5,19 +5,20 @@ import { NavLink } from 'react-router-dom';
 
 class ApplicantProfile extends Component {
     state = {
-        applicant : getApplicantById("11111") ,
-        document : [] ,
+        applicant : {},
+        applicant : [] ,
     };
 
 
     componentDidMount() {
 
-        fetch("http://localhost:4000/getApplicant/:applicantId/search", {
-        body:JSON.stringify({"data":{"applicantId":this.state.applicantId}}),
-        headers:{"Content-Type" : "application/json","x-auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ5YXJ3aXR6Iiwib3JnYW5pemF0aW9uIjoiT3JnMU1TUCIsInJvbGUiOiJ2aWNlQWRtaW4iLCJpYXQiOjE2NTAzMDkwMjB9.0M-GGJicvYNRt4JRYtzVjayIXosWkwq4D2nrySStRac"}
+        fetch("http://localhost:4000/getMyDetails", {
+        method:"POST",
+        // body:JSON.stringify({"data":{"applicantId":this.state.applicantId}}),
+        headers:{"Content-Type" : "application/json","x-auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkcmV2YW50ZW0xIiwib3JnYW5pemF0aW9uIjoiT3JnMU1TUCIsInJvbGUiOiJhcHBsaWNhbnQiLCJpYXQiOjE2NTAyOTM4MjJ9.S4wiaq_eg_ji3YRoj9ParN6TOspvSLgIWEzHvV-Ji-c"}
         })
         .then(response => response.json())
-        .then((data) => this.setState({documents:data}))
+        .then((data) => this.setState({applicant:data}))
     }
     
     render() {
@@ -29,23 +30,23 @@ class ApplicantProfile extends Component {
                     <center><h4> Profile </h4></center><br/>
                     <div className="input-field">
                         <label htmlFor="id">Id</label>
-                        <input type="id" name="id" value={document._id} disabled/>
+                        <input type="id" name="id" value={applicant.applicantId} disabled/>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" value={document.email} disabled/>
+                        <input type="email" name="email" value={applicant.email} disabled/>
                         <label htmlFor="fullName">Name</label>
-                        <input type="text" name="fullName" value={document.name} disabled/>
+                        <input type="text" name="fullName" value={applicant.name} disabled/>
                         <label htmlFor="address">Address</label>
-                        <input type="text" name="address" value={document.address} disabled/>
+                        <input type="text" name="address" value={applicant.address} disabled/>
                         <label htmlFor="pincode">Pin/Zip Code</label>
-                        <input type="text" name="pincode" value={document.pin} disabled/>
+                        <input type="text" name="pincode" value={applicant.pin} disabled/>
                         <label htmlFor="state">State</label>
-                        <input type="text" name="state" value={document.state} disabled/>
+                        <input type="text" name="state" value={applicant.state} disabled/>
                         <label htmlFor="country">Country</label>
-                        <input type="text" name="country" value={document.country} disabled/>
-                        <label htmlFor="contactNumber">Mobile Number</label>
-                        <input type="number" name="contactNumber" value={document.contactNumber} disabled/>
+                        <input type="text" name="country" value={applicant.country} disabled/>
+                        <label htmlFor="contact">Mobile Number</label>
+                        <input type="text" name="contact" value={applicant.contact} disabled/>
                         <label htmlFor="dob">Date of Birth</label>
-                        <input type="date" name="dob" value={document.dateOfBirth} disabled/>
+                        <input type="text" name="dob" value={applicant.dateOfBirth} disabled/>
 
                         <NavLink className="nav-link" to="/applicant/profileUpdate">
                         <input type="submit" value="UPDATE" className="button" /></NavLink>
