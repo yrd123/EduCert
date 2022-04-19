bb9864d2b9c25c60d1b0aa8b518db9502cdf4936
yrd123 <yash.deorah@somaiya.edu>
2022-04-19 23:29:53 +0530

    login connection

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,14 +1,14 @@
-export async function login(loginCredentials){
+export function login(loginCredentials){
     console.log(loginCredentials)
     let token = "";
-    const promise = await fetch("http://localhost:4000/login", {
+    fetch("http://localhost:4000/login", {
     method:"POST",
     headers:{"Content-Type" : "application/json"},
     body:JSON.stringify(loginCredentials)
-    })
+    }).then(response => console.log(response.text()))
     // .then(response => console.log(response.json()))
     // .then(data => console.log(data));
-    console.log(promise)
+    
     return token;
     //localStorage.setItem('eduCertJwtToken', data)
     //console.log(localStorage.getItem('eduCertJwtToken'));
