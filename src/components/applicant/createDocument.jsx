9c56c3c7f097150112182e964bb40398e0ffbbb1
pointres <ronakdesai159@gmail.com>
2022-04-26 01:29:58 +0530

    Document Testing 3

--- src/components/applicant/createDocument.jsx
+++ src/components/applicant/createDocument.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 class CreateSelfUploadedDocument extends Component {
     state = {
