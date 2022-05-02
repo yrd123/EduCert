0e996508389079f67025d9480f590686156c5bde
manish103p <manish.parihar@somaiya.edu>
2022-05-02 14:23:29 +0530

    css

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -35,15 +35,24 @@ class Home extends Component {
           
           <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" />
         
-          <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+          {/* <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
           <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
           <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
           <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
           <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
           <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
-          <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+          <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" /> */}
+
+<link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet' />
+<link href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css" rel="stylesheet" />
+
+
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"/>
+
           {/* Template Main CSS File */}
-          <link href="assets/css/style.css" rel="stylesheet" />
+          {/* <link href="assets/css/style.css" rel="stylesheet" /> */}
           {/* ======= Hero Section ======= */}
           <section id="hero" className="d-flex align-items-center">
             <div className="container">
